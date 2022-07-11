f42c9a5010de4334a8779785841596fb02a4ca04
roshan.maharjan <roshan.maharjan>
2022-07-11 17:29:53 +0545

    Django Model Instance as API Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,24 +2,14 @@ import json
 
 from django.http import JsonResponse
 
+from products.models import Product
 
 def api_home(request, *args, **kwargs):
-    # request -> HttpRequest -> Django
-    # print(dir(request))
-    # request.body
-    # print(request.GET) # URL query params
-    # print(request.POST)
-    body = request.body # byte string of JSON data
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body) # string of JSON data -> Python Dict
-    except:
-        pass
-    # print(data)
-    # print(request.headers)
-    # json.dumps(request.headers) # Gives error
-    data['params'] = request.GET
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
-    print(data)
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,13 +1,16 @@
 import requests
 
-endpoint = "http://httpbin.org/status/200"
+# endpoint = "http://httpbin.org/status/200"
 # endpoint = "http://httpbin.org/anything"
+# endpoint = "http://localhost:8000/" # http://127.0.0.1:8000/
+endpoint = "http://localhost:8000/api/" # http://127.0.0.1:8000/api/
 
-get_response = requests.get(endpoint, json={"query":"hello world"}) #Application Programming Interface
-# print(get_response.text)
+get_response = requests.get(endpoint, params={'abc':123}, json={"query":"hello world"}) #Application Programming Interface
 
 # HTTP Requests -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Notation ~ Python Dict
 
-print(get_response.status_code)
+# print(get_response.text)
+# print(get_response.status_code)
+print(get_response.json())
