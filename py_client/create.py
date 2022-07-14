702b5733b829aa1cbf3ed90c4e629186cdcf4f45
roshan.maharjan <roshan.maharjan>
2022-07-14 11:42:50 +0545

    Token Authentication

--- py_client/create.py
+++ py_client/create.py
@@ -1,13 +1,16 @@
 import requests
 
+headers = {
+    'Authorization': 'Bearer 715138dc4a7e79249ec877147f3f6b8fc4e24b3a'
+}
 
 endpoint = "http://localhost:8000/api/products/"
 
 data = {
-    'title':'This is from create MIXIN',
+    'title':'This is from TOKEN Creation Again',
     'price':18.22
 }
 
-get_response = requests.post(endpoint, json=data) 
+get_response = requests.post(endpoint, json=data, headers=headers) 
 
 print(get_response.json())
--- py_client/list.py
+++ py_client/list.py
@@ -1,8 +1,22 @@
 import requests
+from getpass import getpass
 
 
-endpoint = "http://localhost:8000/api/products/"
+auth_endpoint = "http://localhost:8000/api/auth/"
+username = input("What's your username? ")
+password = getpass("What's your password? ")
 
-get_response = requests.get(endpoint) 
+auth_response = requests.post(auth_endpoint, json={'username': username, 'password':password}) 
 
-print(get_response.json())
+print(auth_response.json())
+
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        'Authorization': f"Bearer {token}"
+    } 
+    endpoint = "http://localhost:8000/api/products/"
+
+    get_response = requests.get(endpoint, headers=headers) 
+
+    print(get_response.json())
